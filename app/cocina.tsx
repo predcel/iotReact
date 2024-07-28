c2db8b1cc5cecd61d7a8bba3f9fd2b34ca31a7b8
predcel <predcel@hotmail.com>
2024-07-27 21:43:21 -0600

    cocina lista

--- app/cocina.tsx
+++ app/cocina.tsx
@@ -1,25 +1,70 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, ScrollView, Switch } from "react-native";
-import { Avatar } from '@rneui/themed';
-import { Text } from '@rneui/themed';
+import { Dialog, Text, Avatar } from '@rneui/themed';
+
+const randomNum = (max = 0) => {
+    const aux = Math.random();
+    return Math.floor(aux * max);
+}
 
 export default function Cocina() {
+    // const [isEnabled, setIsEnabled] = useState(randomNum(2) == 1 ? true : false);
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const switchOn = () => setIsEnabled(isEnabled ? previousState => previousState : previousState => !previousState);
+
+    const [visible5, setVisible5] = useState(randomNum(2) == 1 ? true : false);
+    const toggleDialog5 = () => {
+        setVisible5(!visible5);
+    };
+
     return (
         <ScrollView>
             <View style={estilos.renglon}>
                 <Avatar
                     size={200}
                     rounded
-                    icon={{ name: 'kitchen', type: 'material', color: '#009688' }}
+                    icon={{ name: 'blender', type: 'material', color: '#009688' }}
                     containerStyle={{
                         borderColor: 'grey',
                         borderStyle: 'solid',
                         borderWidth: 3,
                     }} />
-            </View>            
+            </View>
+            <Text style={estilos.header}>Estractor de humo</Text>
+            <View style={estilos.renglon}>
+                <Text style={estilos.espacioDerecho}>Apagado</Text>
+                <Switch
+                    trackColor={{ false: '#767577', true: '#81b0ff' }}
+                    thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                />
+                <Text style={estilos.espacioIzquierdo}>Encendido</Text>
+            </View>
+            <Dialog
+                isVisible={visible5}
+                onBackdropPress={toggleDialog5}
+            >
+                <Dialog.Title
+                    title="Se ha detectado humo en la habitacion. 
+                ¿Desea encender el estractor?" />
+                <Dialog.Actions>
+                    <Dialog.Button title="Salir" onPress={toggleDialog5} />
+                    <Dialog.Button
+                        title="Encender"
+                        onPress={() => {
+                            switchOn();
+                            toggleDialog5();
+                        }}
+                    />
+                </Dialog.Actions>
+            </Dialog>
         </ScrollView>
     );
 }
+
 
 const estilos = StyleSheet.create({
     basicStyle: {
